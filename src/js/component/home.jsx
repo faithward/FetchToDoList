5a60051c95bff37bf1929fcbb75518f3d38e54da
faithward <faithward24@gmail.com>
2022-10-08 01:26:10 +0000

    added user input

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,25 @@
-import React, { useEffect , useState} from "react";
+import React, { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
-	const [list, setList] = useState([])
+  const [list, setList] = useState([]);
   useEffect(() => {
-    getList()
+    getList();
   }, []);
 
   const getList = () => {
-	fetch("https://assets.breatheco.de/apis/fake/todos/user/faithward")
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/faithward")
       .then((response) => response.json())
       .then((result) => setList(result))
       .catch((error) => console.log("error", error));
-  }
+  };
 
   const addTask = (myTask) => {
-	const newList = [...list, myTask];
+    const newEntry = {label: myTask , done: false}
+    const newList = [...list, newEntry];
     fetch("https://assets.breatheco.de/apis/fake/todos/user/faithward", {
       method: "PUT",
-      headers: {"Content-Type": "application/json"},
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newList),
       redirect: "follow",
     })
@@ -26,17 +27,31 @@ const Home = () => {
       .then((result) => getList())
       .catch((error) => console.log("error", error));
   };
+  
+  const [newTask, setNewTask] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+  };
+
   return (
     <div className="text-center">
-		{list.map((task, i) => {
-			return(
-				<p key={i}>{task.label}</p>
-			)
-		})}
-		<button className="btn btn-primary"
-		onClick={() => addTask({ label: "Run", done: false})}>
-			Add
-		</button>
+      <form onSubmit={handleSubmit}>
+       <h2><label>
+          What do you need to do?<br></br>
+          <input
+            type="text"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+        </label></h2>
+        <input type="submit" />
+      </form>
+      <br></br>
+      {list.map((task, i) => {
+        return <p key={i}>{task.label}</p>;
+      })}
     </div>
   );
 };
