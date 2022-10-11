f0bf5afc0f4a600d5cce5d606790b08bab978213
faithward <faithward24@gmail.com>
2022-10-11 00:49:17 +0000

    added clear function

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   };
 
   const addTask = (myTask) => {
-    const newEntry = {label: myTask , done: false}
+    const newEntry = { label: myTask, done: false };
     const newList = [...list, newEntry];
     fetch("https://assets.breatheco.de/apis/fake/todos/user/faithward", {
       method: "PUT",
@@ -27,31 +27,54 @@ const Home = () => {
       .then((result) => getList())
       .catch((error) => console.log("error", error));
   };
-  
+
+  const clearList = () => {
+    const tempEntry = { label: " ", done: false };
+    const tempList = [tempEntry];
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/faithward", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(tempList),
+      redirect: "follow",
+    })
+      .then((response) => response.json())
+      .then((result) => getList())
+      .catch((error) => console.log("error", error));
+  };
+
   const [newTask, setNewTask] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
     addTask(newTask);
     setNewTask("");
   };
+  
+  const handleClear = (event) => {
+    event.preventDefault();
+    clearList();
+    setNewTask("");
+  }
 
   return (
     <div className="text-center">
       <form onSubmit={handleSubmit}>
-       <h2><label>
-          What do you need to do?<br></br>
-          <input
-            type="text"
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-          />
-        </label></h2>
+        <h2>
+          <label>
+            What do you need to do?<br></br>
+            <input
+              type="text"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+            />
+          </label>
+        </h2>
         <input type="submit" />
       </form>
       <br></br>
       {list.map((task, i) => {
         return <p key={i}>{task.label}</p>;
       })}
+      <button onClick={handleClear}>Clear list</button>
     </div>
   );
 };
